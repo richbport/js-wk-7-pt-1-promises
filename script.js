c8a3afb11cfdfb5d459d533b5af3a6b7cf1d241c
Rich <richbport@yahoo.com>
2025-10-01 13:17:15 -0400

    async await response

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(fetch('https://jsonplaceholder.typicode.com/users/1'))
+// console.log(fetch('https://jsonplaceholder.typicode.com/users/1'))
+
+async function main() {
+    const response = await (fetch('https://jsonplaceholder.typicode.com/users/1'));
+    console.log(response);
+}
+
+main();
