2bfb8380f50b6400f44c5ecd5870b5b584b8ce72
Rich <richbport@yahoo.com>
2025-10-02 14:08:16 -0400

    try catch

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 // console.log(fetch('https://jsonplaceholder.typicode.com/users/1'))
 
-async function main() {
-    const response = await (fetch('https://jsonplaceholder.typicode.com/users/1')); // Fetch the data from an API
-    const data =  await response.json() // parse the JSON data
-    console.log(data); // console.log the result
-}
+// async function main() {
+//     const response = await (fetch('https://jsonplaceholder.typicode.com/users/1')); // Fetch the data from an API
+//     const data =  await response.json() // parse the JSON data
+//     console.log(data); // console.log the result
+// }
 
-main(); // Call the function
+// main(); // Call the function
+
+async function main() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
+    }
+
+    if (!response.ok) {
+        throw new Error(`HTTP error! Status ${response.status}`);
+    }
+}
