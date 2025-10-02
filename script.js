a4a64019b222068b6e42ca3a54ae73576e0cc3f4
Rich <richbport@yahoo.com>
2025-10-02 14:15:24 -0400

    format adjust

--- script.js
+++ script.js
@@ -9,18 +9,20 @@
 // main(); // Call the function
 
 async function main() {
-    try {
-        const response = await fetch('https://jsonplaceholder.typicode.com/users/1');
-    }
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/users/1"
+    );
 
     if (!response.ok) {
-        throw new Error(`HTTP error! Status ${response.status}`);
+      throw new Error(`HTTP error! Status ${response.status}`);
     }
 
     const data = await response.json();
     console.log("User Data", data);
-} catch (error) {
+  } catch (error) {
     console.error("Error occured while fetching data:", error.message);
+  }
 }
 
 main();
