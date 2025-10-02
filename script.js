ae5d5e6d52f5314638470d14e0780312f737fc45
Rich <richbport@yahoo.com>
2025-10-02 14:23:31 -0400

    catch (error)

--- script.js
+++ script.js
@@ -1,28 +1,32 @@
 // console.log(fetch('https://jsonplaceholder.typicode.com/users/1'))
 
-// async function main() {
-//     const response = await (fetch('https://jsonplaceholder.typicode.com/users/1')); // Fetch the data from an API
-//     const data =  await response.json() // parse the JSON data
-//     console.log(data); // console.log the result
-// }
-
-// main(); // Call the function
-
 async function main() {
-  try {
-    const response = await fetch(
-      "https://jsonplaceholder.typicode.com/users/1"
-    );
-
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status ${response.status}`);
+    try {
+        const response = await (fetch('https://jsonplaceholder.typicode.com/users/1'));
+        const data =  await response.json() 
+        console.log(data); 
+    } catch (error) {
+        
     }
-
-    const data = await response.json();
-    console.log("User Data", data);
-  } catch (error) {
-    console.error("Error occured while fetching data:", error.message);
-  }
 }
 
-main();
+main(); 
+
+// async function main() {
+//   try {
+//     const response = await fetch(
+//       "https://jsonplaceholder.typicode.com/users/1"
+//     );
+
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status ${response.status}`);
+//     }
+
+//     const data = await response.json();
+//     console.log("User Data", data);
+//   } catch (error) {
+//     console.error("Error occured while fetching data:", error.message);
+//   }
+// }
+
+// main();
